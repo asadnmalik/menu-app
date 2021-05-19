fc6415f3af009470974547f71798b775460fb0fd
asadnmalik <asadmnaveed@gmail.com>
2021-05-19 21:54:03 +0500

    added theory

---
+++ theory.js
@@ -0,0 +1,15 @@
+// What is the DOM?
+//  Used in html/xml that defines the logical structure of documents where you can manipulate and access them
+
+
+// What is an event?
+// the interaction with the html or dom, a single event may contain page load, clicking, scrolling
+
+// What is an event listener?
+// a method can be used to listen to a specifc event so you can decide or manipulate differenct actions upon the occurance of that event
+
+// Why would we convert a NodeList into an Array?
+// You're unable to use array methods like forEach on a NodeList
+
+// What is a component?
+// component is one section of an interactive UI, in React each component serve as a code and return a rendered HTML code independently.
